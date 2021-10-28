f4720450af1d49130a53090ceaaaf4cfe8441b1a
acroisie <arnaud.croisier@live.fr>
2021-10-28 12:40:43 +0200

    add ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -13,7 +13,24 @@
 int	ft_atoi(char const *str)
 {
 	int	i;
+	int	sign;
+	int	result;
 
 	i = 0;
-	while ()
+	sign = 1;
+	result = 0;
+	while (str[i] >= 0 && str[i] <= 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = sign * -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - 48);
+		i++;
+	}
+	return (result * sign);
 }
