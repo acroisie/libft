074bf8e54a712f9b670a7f128b7f1b703df7201d
acroisie <acroisier@student.42lyon.fr>
2021-11-03 08:08:49 +0100

    add ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,29 +6,32 @@
 /*   By: acroisie <acroisie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 16:07:24 by acroisie          #+#    #+#             */
-/*   Updated: 2021/11/02 17:35:49 by acroisie         ###   ########.fr       */
+/*   Updated: 2021/11/03 08:08:17 by acroisie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
-
-int	ft_strlen(const char *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	int		i;
-	int		len;
+	int		j;
 	char	*str;
 
 	i = 0;
-	len = ft_strlen(s1) + ft_strlen(s2);
-	str = malloc((len * sizeof(char)) + 1);
+	j = 0;
+	str = malloc(((ft_strlen(s1) + ft_strlen(s2)) * sizeof(char)) + 1);
+	while (s1[j])
+	{
+		str[i] = s1[j];
+		j++;
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		str[i] = s2[j];
+		j++;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
