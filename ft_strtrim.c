178da86d44c840b2322d9a733306f4b65760bd32
acroisie <acroisier@student.42lyon.fr>
2021-11-05 12:29:15 +0100

    correct strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -5,71 +5,36 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: acroisie <acroisie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/04 18:40:38 by acroisie          #+#    #+#             */
-/*   Updated: 2021/11/05 09:54:35 by acroisie         ###   ########lyon.fr   */
+/*   Created: 2021/11/05 11:11:01 by acroisie          #+#    #+#             */
+/*   Updated: 2021/11/05 11:37:30 by acroisie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	within_start(const char *str, const char *set)
+static int	c_in_set(char c, const char *set)
 {
 	int	i;
-	int	stop;
 
 	i = 0;
-	stop = ft_strlen(set);
-	while (i < stop)
+	while (set[i])
 	{
-		if (str[i] != set[i])
-			return (0);
+		if (set[i] == c)
+			return (1);
 		i++;
 	}
-	return (1);
-}
-
-static int	within_end(const char *str, const char *set)
-{
-	int	i;
-	int	stop;
-
-	i = ft_strlen(str);
-	stop = ft_strlen(set);
-	while (stop > 0)
-	{
-		if (str[i] != set[stop])
-			return (0);
-		i--;
-		stop--;
-	}
-	return (1);
+	return (0);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	int		i;
-	int		sub_to_len;
-	int		set_len;
-	int		nstr_len;
-	char	*new_string;
+	int	i;
+	int	len_to_sub;
 
 	i = 0;
-	sub_to_len = 0;
-	set_len = ft_strlen(set);
-	if (within_start(s1, set))
-		sub_to_len = set_len;
-	if (within_end(s1, set))
-		sub_to_len = sub_to_len + set_len;
-	nstr_len = ft_strlen(s1) - sub_to_len;
-	new_string = malloc((nstr_len + 1) * sizeof(char));
-	if (new_string == NULL)
-		return (NULL);
-	while (i < nstr_len)
+	while (c_in_set(s1[i], set))
 	{
-		new_string[i] = s1[set_len];
-		set_len++;
 		i++;
+		len_to_sub++;
 	}
-	new_string[i] = '\0';
-	return (new_string);
 }
