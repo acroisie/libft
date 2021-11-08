965a5ec74e548478e47c5929568c5bd0b4ff390f
acroisie <acroisier@student.42lyon.fr>
2021-11-08 11:17:25 +0100

    add_clean_ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: acroisie <acroisie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 11:11:01 by acroisie          #+#    #+#             */
-/*   Updated: 2021/11/05 11:37:30 by acroisie         ###   ########lyon.fr   */
+/*   Updated: 2021/11/08 11:16:28 by acroisie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,41 @@ static int	c_in_set(char c, const char *set)
 	return (0);
 }
 
-char	*ft_strtrim(char const *s1, char const *set)
+static char	*fill_str(const char *s1, int start, int end, int len_to_sub)
 {
-	int	i;
-	int	len_to_sub;
+	int		i;
+	char	*new_str;
 
 	i = 0;
-	while (c_in_set(s1[i], set))
+	new_str = malloc((end - len_to_sub + 1) * sizeof(char));
+	if (new_str == NULL)
+		return (NULL);
+	while (start < end)
 	{
+		new_str[i] = s1[start];
 		i++;
-		len_to_sub++;
+		start++;
 	}
+	new_str[i] = '\0';
+	return (new_str);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int		end;
+	int		len_to_sub;
+	int		start;
+	char	*new_str;
+
+	len_to_sub = 0;
+	end = ft_strlen(s1);
+	while (c_in_set(s1[len_to_sub], set))
+		len_to_sub++;
+	if (len_to_sub == end)
+		return (ft_calloc(1, sizeof(char)));
+	start = len_to_sub;
+	while (c_in_set(s1[end - 1], set))
+		end--;
+	new_str = fill_str(s1, start, end, len_to_sub);
+	return (new_str);
 }
