86cb7c7f4e2907d309ab6500f2464b6a78f6c48b
acroisie <arnaud.croisier@live.fr>
2022-01-14 10:09:01 +0100

    update_ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: acroisie <acroisie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 16:07:24 by acroisie          #+#    #+#             */
-/*   Updated: 2022/01/14 08:31:55 by acroisie         ###   ########lyon.fr   */
+/*   Updated: 2022/01/14 10:08:28 by acroisie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	i = 0;
 	j = 0;
 	if (!s1)
-		return ((char *)s2);
+		return (ft_strdup((char *)s2));
 	if (!s2)
-		return ((char *)s1);
+		return (ft_strdup((char *)s1));
 	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
@@ -45,9 +45,9 @@ char	*ft_strjoin_free_s1(char *s1, char *s2)
 	i = 0;
 	j = 0;
 	if (!s1)
-		return (s2);
+		return (ft_strdup(s2));
 	if (!s2)
-		return (s1);
+		return (ft_strdup(s1));
 	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
@@ -70,9 +70,9 @@ char	*ft_strjoin_free_s2(char *s1, char *s2)
 	i = 0;
 	j = 0;
 	if (!s1)
-		return (s2);
+		return (ft_strdup(s2));
 	if (!s2)
-		return (s1);
+		return (ft_strdup(s1));
 	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
